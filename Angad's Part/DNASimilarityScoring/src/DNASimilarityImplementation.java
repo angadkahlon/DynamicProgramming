98cc8524da4c13debd1dca040c173e0b9cfedad4
Angad Kahlon <angad.kah@gmail.com>
2024-04-06 15:15:18 -0700

    Created a class, attributes, constructor, for the java implementation. Added an explanation about the project as a comment as well.

---
+++ Angad's Part/DNASimilarityScoring/src/DNASimilarityImplementation.java
@@ -0,0 +1,33 @@
+//Angad Kahlon, Student #: 300187930, 
+//COMP 359 - Design and Analysis of Algorithms - Professor: Dr. Russell Campbell
+
+/* Explanation:
+ * This is the code implementation for the final portion of the course COMP 359 
+ * (Design and Analysis of Algorithms). 
+ * 
+ * I chose to implement a topic that we learned about in COMP 359
+ * that I found very interesting which is about how Dynamic Programming can be used
+ * in find similarities between DNA Sequences. The code below is a Java implemenation
+ * of this idea.
+ * 
+ * I would like to first give credit and reference the people who explained and 
+ * implemented this in Python. I took inspiration and used their implemenation as a
+ * resource to implement my version.
+ * 
+ * Treadway, A. (2018, November 28). 
+ * How to measure DNA similarity with Python and Dynamic Programming - Open Source Automation. Open Source Automation. 
+ * https://theautomatic.net/2018/11/28/how-to-measure-dna-similarity-with-python-and-dynamic-programming/
+ */
+
+public class DNASimilarityImplementation {
+
+    private String X = new String("GTCCATACA");
+    private String Y = new String("TCATATCAG");
+
+    public DNASimilarityImplementation(String x, String y){
+        X = x;
+        Y = y;
+    }
+
+}
+
